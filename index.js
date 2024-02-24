80c906cc306e4e654c513cd02da317bc750753be
bhc <behicsakar@gmail.com>
2024-02-24 14:16:58 +0300

    Add version flag to display CLI version

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
 const readline = require("readline");
+const { version } = require("./package.json");
+
+// Check if the user passed the --version flag
+if (process.argv.includes("--version")) {
+  console.log(`DrinkTea CLI version ${version}`);
+  process.exit(0);
+}
 
 const rl = readline.createInterface({
   input: process.stdin,
